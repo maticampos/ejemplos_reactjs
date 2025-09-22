9cf06e6df6eac2e862fb359c29b3b8110b2e12ca
Matías Ignacio Campos <89554561+maticampos@users.noreply.github.com>
2025-09-22 18:05:24 -0300

    Se añaden comentarios al CarritoSimple.jsx

--- Clase4/src/ContadorSimple.jsx
+++ Clase4/src/ContadorSimple.jsx
@@ -1,32 +1,50 @@
-{/* useState con evento onClick */}
-
 import { useState } from 'react';
 
-function Contador() {
-  const [contador, setContador] = useState(0);
+function EcommerceSimple() {
 
-  return (
-    <div id="tarjeta">
-      <h5>useState con evento onClick</h5>
-      <h1>Contador simple</h1>
-      <p>{contador}</p>
-      <button onClick={() => setContador(contador + 1)}>Contador</button>
-    </div>
-  );
-} export default Contador
+  {/* Lista de Productos */} 
+    const listaProductos = [
+    { id: 1, nombre: 'Camiseta', precio: 15.000 },
+    { id: 2, nombre: 'Pantalón', precio: 30.000 },
+    { id: 3, nombre: 'Zapatos', precio: 50.000 },
+    ];
 
-{/* 
+    {/* Variable de estado carrito*/}
+    const [carrito, setCarrito] = useState([]);
+
+    {/* Función Agregar Carrito*/}
+    const agregarCarrito = (producto) => {
+      setCarrito([...carrito, producto]);
+    };
+
+    { /* Función Vaciar Carrito */}
+    const vaciarCarrito = () => {
+      setCarrito([]);
+    };
+
+    {/* Retorno UI o gráfico del componente */}
+
+    return (
+      <div id="tarjeta">
+        <h3>Productos</h3>
+        <ul>
+          {listaProductos.map((producto) => ( // aplica método .map() sobre la lista "listaProductos" y ewl parámetro "producto" es la variable de iteración que recorre cada objeto de la lista.
+            <li key={producto.id}> {/* use prducto.id como clave de identificación única del objeto*/}
+              {producto.nombre}: ${producto.precio.toFixed(3)} {/* Muestra adentro del <li> el nombre del producto y el precio.con(3)decimales */}
+              <button onClick={() => agregarCarrito(producto)}>Agregar</button> {/* El botón "Agregar contiene el llamado a la función "agregarCarrrito" cuando se hace onClick sobre él que modifica la cariable carrito con setCarrito, agregando el producto del botón que se haga click */}
+            </li>
+          ))}
+        </ul>
+        <h2>=============================</h2>
+        <h3>Carrito</h3>
+        {carrito.map((producto) => ( // aplica .map() sobre la variable de estado carrito para mostrar los productos fueron agregados.
+          <p key={producto.id}>
+            {producto.nombre}: ${producto.precio.toFixed(3)}
+          </p>
+        ))}
+        <button onClick={vaciarCarrito}>Eliminar</button> {/* invoca a la función eliminar carrito que sobrescribe un array vación ([]) eliminando todo lo almacenado */}
+      </div>
+    );
+  }; export default EcommerceSimple;
 
 
-
-const [contador, setContador] = useState(valorInicial);
-  Es la variable de estado.
-  
-contador: es variable con el valor actual o incial del estado. 
-
-setContador: 
-es la función que actualiza el estado y ejecuta el nuevo renderizado del componente.
-
-valorInicial: el valor inicial del estado (puede ser un primitivo(string, número, Bool), objeto, array, etc.).
-
-*/ }
