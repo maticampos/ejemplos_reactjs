c2bf0f0c280c07808fc60f63324d1bcf95b9bc19
Matías Ignacio Campos <mati.campos@bue.edu.ar>
2025-10-02 00:57:52 -0300

    Clase #6 con React Router

---
+++ Clase6/test6/src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Inicio() {
+  return (
+    <>  
+    <h1>Bienvenidos al Incio</h1>
+    </>
+  )
+}
+
+export default Inicio
---
+++ Clase6/test6/src/pages/Productos.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Productos() {
+  const [productos, setProductos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://68d482e3214be68f8c696ae2.mockapi.io/api/productos")
+      .then((respuesta) => respuesta.json())
+      .then((datos) => {
+        setProductos(datos);
+        setCargando(false);
+      })
+      .catch((error) => {
+        {console.error("Error!,", error)}
+        setError("Hubo un problema al cargar los productos.");
+        setCargando(false);
+      });
+  }, []);
+
+  if (cargando) return <p>Cargando productos...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <ul id="lista-productos">
+      {productos.map((producto) => (
+        <li key={producto.id}>
+        <h2>{producto.nombre}</h2>
+          <br />
+          Descripción: {producto.descripcion}
+          <br />
+          Precio: ${producto.precio}
+          <br />
+          <img src={producto.avatar} alt={producto.nombre} width="80%" />
+          <Link to={`/productos/${producto.id}`} state={{producto}}><button>Más detalles</button></Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
